73b9ebfc46672ad55d43c00d3ae1fa7d169be8c0
tiornac <tiorna@outlook.fr>
2022-10-20 07:38:31 -0700

    The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - fizz buzz test.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i = 1;
+while (i <= 100)
+{
+if (i % 3 == 0 && !(i % 5 == 0))
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0 && !(i % 3 == 0))
+{
+printf("Buzz ");
+}
+else if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+i++;
+}
+printf("\n");
+return (0);
+}
